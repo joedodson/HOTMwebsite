b781f266bd727225760d39c52b9c3f095a1629a6
Joseph Dodson <joseph.dodson@ucdenver.edu>
2018-03-05 22:23:32 -0700

    started implementation of hamburger menu; resized homepage cards

--- src/app/reg-menu/reg-menu.component.ts
+++ src/app/reg-menu/reg-menu.component.ts
@@ -1,9 +1,22 @@
 import { Component, OnInit, HostListener } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-reg-menu',
   templateUrl: './reg-menu.component.html',
-  styleUrls: ['./reg-menu.component.scss']
+  styleUrls: ['./reg-menu.component.scss'],
+  animations: [
+    trigger('slideInOut', [
+      state('in', style({
+        transform: 'translate3d(0,0,0)'
+      })),
+      state('out', style({
+        transform: 'translate3d(100%, 0, 0)'
+      })),
+      transition('in => out', animate('400ms ease-in-out')),
+      transition('out => in', animate('400ms ease-in-out'))
+    ]),
+  ]
 })
 
 export class RegMenuComponent implements OnInit {
@@ -19,6 +32,7 @@ export class RegMenuComponent implements OnInit {
   
   navbar = undefined;
   navOffset = undefined;
+  menuState = 'in';
   @HostListener('window:scroll', []) onWindowScroll(){
     if(window.pageYOffset >= this.navOffset){
       this.navbar.classList.add("sticky");
@@ -26,6 +40,19 @@ export class RegMenuComponent implements OnInit {
     else{
       this.navbar.classList.remove("sticky");
     }
+  }
+
+  menuClick():void{
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
+    var elem = document.getElementById("smc");
+    if(this.menuState === 'out'){
+      var height = window.innerHeight;
+      elem.setAttribute("style", "width:300px; height:" + height + "px");
+    }
+    else{
+      elem.setAttribute("style", "width:50px; height:50px");
+    }
+    document.getElementById("mbc").classList.toggle("change");
   }
 
   constructor() { 
