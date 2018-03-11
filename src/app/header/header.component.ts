3a42dd8e00e1778df10c97d9d33239a24a03337c
Joseph Dodson <joseph.dodson@ucdenver.edu>
2018-03-10 21:10:01 -0700

    redo of menu format, above hero image

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  links = [
+    {url:"/home",text:"Home"},
+    {url: "/services", text: "Services"},
+    {url: "/products", text: "Products"},
+    {url: "/gallery", text: "Gallery"},
+    {url: "/about", text: "About Us"},
+    {url: "/contact", text: "Contact"}
+  ]
+  
+  navbar = undefined;
+  navOffset = undefined;
+  menuState = 'in';
+  @HostListener('window:scroll', []) onWindowScroll(){
+    if(window.pageYOffset >= 25){
+      this.navbar.classList.add("sticky");
+    }
+    else{
+      this.navbar.classList.remove("sticky");
+    }
+  }
+
+  menuClick():void{
+    this.menuState = this.menuState === 'out' ? 'in' : 'out';
+    var elem = document.getElementById("smc");
+    if(this.menuState === 'out'){
+      var height = window.innerHeight;
+      elem.setAttribute("style", "background-color: rgba(61, 61, 61, 1); width:290px; height:" + height + "px; right:0; top:0px");
+    }
+    else{
+      elem.setAttribute("style", "width:50px; height:50px; right:15px; top:25px");
+    }
+    document.getElementById("mbc").classList.toggle("change");
+  }
 
   constructor() { }
 
   ngOnInit() {
+    this.navbar = document.getElementsByClassName("header")[0];
+    this.navOffset = this.navbar.offsetTop;
   }
 
 }
